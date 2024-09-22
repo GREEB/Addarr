ea7c197e6303f1480a7e1944943a023914a5fb2a
GREEB <1221769+GREEB@users.noreply.github.com>
2024-09-22 20:06:21 +0200

    Update commons.py

--- src/commons.py
+++ src/commons.py
@@ -21,7 +21,10 @@ def generateServerAddr(app):
             addr = config[app]["server"]["addr"]
             port = config[app]["server"]["port"]
             path = config[app]["server"]["path"]
-            return http + addr + ":" + str(port) + path
+            if port:  # Check if the port is not empty or None
+                return f"{http}{addr}:{port}{path}"
+            else:
+                return f"{http}{addr}{path}"
         except Exception:
             logger.warn("No ip or port defined.")
     except Exception as e:
